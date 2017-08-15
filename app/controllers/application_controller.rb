4e2051430be2d6b1b127e8e1ae19f3c269aa9a51
Charles Krywko <ckrywko@chicagobooth.edu>
2017-08-15 02:46:11 +0000

    more devise stuff done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!    # locks down app unless a user is signed in
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :avatar_url])
+  end
+
 end
